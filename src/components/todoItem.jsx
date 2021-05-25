afe4bb4ee41875aa965db4730be599b769348806
Artyom freezyng <tema9651@yandex.ru>
2021-05-25 17:58:05 +0300

    fix styles

--- src/components/todoItem.jsx
+++ src/components/todoItem.jsx
@@ -3,12 +3,14 @@ import './todoItem.css';
 
 function TodoItem(props) {
     return(
-        <div>
-            <div className="row item-row">
+        <div className="row">
+            <div className="item-row">
                 <div className="id">#{props.todo.id}</div>
                 <div className="title">{props.todo.message}</div>
-                <button className="btn btn-edit">изменить</button>
-                <button className="btn btn-delete" onClick={() => props.deleteTodoThunk(props.todo.id)}>удалить</button>
+                <div className="wrap-btns">
+                    <button className="btn btn-edit">изменить</button>
+                    <button className="btn btn-delete" onClick={() => props.deleteTodoThunk(props.todo.id)}>удалить</button>
+                </div>
             </div>
         </div>
     )
